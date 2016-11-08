3063321bdcddee36af2d6ef12193b25bbb617e21
Mauricio <mauricio.guzman.salazar@gmail.com>
2016-11-07 20:17:00 -0500

    added objetive function verification, added bounds input

--- metodo_1/metodo1.m
+++ metodo_1/metodo1.m
@@ -1,7 +1,27 @@
 clear;
 clc;
-syms x1 x2 obj rest;
+%-------------------variables simbolicas
+syms x1 x2 obj rest restli;
+%-------------------fin variables simbolicas
+%-------------------lectura y verificación de función objetivo
 obj = sym(input('Por favor digite z : ', 's'));
+degree = feval(symengine, 'degree', obj);
+punto = input('Digite un punto Xi arbitrario de la forma [a,b]: ');
+if degree > 1
+    fprintf('Hay que linealizar la función objetivo \n');
+    restli(j) = sym(subs(obj,[x1 x2],punto) + subs(gradient(obj)',[x1 x2], punto)*([x1 x2]'-punto' ))
+else
+    lb = [punto(1),punto(1)];
+    ub = [punto(2),punto(2)];
+    f = double(coeffs(obj));
+    A=[];
+    b=[];
+    Aeq=[];
+    beq=[];
+    X = linprog(f,A,b,Aeq,beq,lb,ub);
+    X = round(X)'
+end
+%-------------------fin lectura y verificación de función objetivo
 fprintf('Por favor digite las restricciones en el siguiente formato :\n');
 fprintf('ejemplo [x1^2 + 3; x1 + 2 + x2] :\n');
 rest = sym(input('Puede digitar las restricciones aqui : ', 's'));
@@ -11,28 +31,20 @@ fprintf('funcion objetivo : \n');
 pretty(obj);
 fprintf('sujeto a :\n');
 pretty(rest);
+j=0;
 for i=1:length(rest)
     degree = feval(symengine, 'degree', rest(i));
     if degree > 1
+        j = j + 1; 
         fprintf('Hay que linealizar la siguiente ecuacion : \n');
         pretty(rest(i));
         % Voy a llamar la funcion para linelizar
+        restli(j) = sym(subs(rest(i),[x1 x2],X) + subs(gradient(rest(i))',[x1 x2], X)*([x1 x2]'-X' ))
     end
 end
 input('para continuar presione cualquier caracter : ', 's')
-clc;
 g = 3*x1^2 -2*x1*x2 + x2^2 - 1;
 obj = x1-x2
-f = [1,-1];
-A=[];
-b=[];
-Aeq=[];
-beq=[];
-lb = [-2,-2];
-ub = [2,2];
-x = linprog(f,A,b,Aeq,beq,lb,ub)
-x = round(x)'
-neweq =  sym(subs(g,[x1 x2],x) + subs(gradient(g)',[x1 x2], x)*([x1 x2]'-x' ))
 c = coeffs(neweq)
 A = double([c(3) c(2)])
 b = double([-c(1)])
