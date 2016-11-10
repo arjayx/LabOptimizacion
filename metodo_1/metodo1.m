b2ccd1ae62b8f4605bfa33f4b0fa6b30da4243b5
jose daza <jose daza>
2016-11-10 18:18:08 -0500

    modify method1

--- metodo_1/metodo1.m
+++ metodo_1/metodo1.m
@@ -3,7 +3,6 @@
 % Roberto Daza, Mauricio Guzman, Alexander Taborda.
 % 2016
 %SLP SEQUENTIAL LINEAR PROGRAMMING
-tic
 clear;
 clc;
 %-------------------variables simbolicas
@@ -16,16 +15,15 @@ punto = input('Digite un punto Xi arbitrario de la forma [a,b]: ');
 if degree > 1
     fprintf('Hay que linealizar la función objetivo \n');
     obj = sym(subs(obj,[x1 x2],punto) + subs(gradient(obj)',[x1 x2], punto)*([x1 x2]'-punto' ))
-else
-    lb = [punto(1),punto(1)];
-    ub = [punto(2),punto(2)];
-    f = fliplr(double(coeffs(obj)));
-    A=[];
-    b=[];
-    Aeq=[];
-    beq=[];
-    X = round(linprog(f,A,b,Aeq,beq,lb,ub),4)'
 end
+lb = [punto(1),punto(1)];
+ub = [punto(2),punto(2)];
+f = fliplr(double(coeffs(obj)));
+A=[];
+b=[];
+Aeq=[];
+beq=[];
+X = round(linprog(f,A,b,Aeq,beq,lb,ub),4)'
 %-------------------fin lectura y verificación de función objetivo
 fprintf('Por favor digite las restricciones en el siguiente formato :\n');
 fprintf('ejemplo [x1^2 + 3<= 3; x1 + 2 + x2 <= 4 ] :\n');
@@ -37,6 +35,7 @@ pretty(obj);
 fprintf('sujeto a :\n');
 pretty(rest);
 fprintf('con una tolerancia de: %d\n',tol);
+tic
 %-----------------------tratamiento y linealización de las restricciones
 for i=1:length(rest)
     [c,matches] = strsplit(char(rest(i)),'\s*<=|>=|=\s*','DelimiterType','RegularExpression');%separamos la ecuacion
